4322a90f7f0b4dfbff60ac3c9c2e1c4a8eb0016e
Boutaina <boutaina.elatbaoui@gmail.com>
2023-12-27 09:18:40 +0100

    Added the rest of the CRUD operations

--- src/main/java/com/example/task_flow/service/implementations/UserServiceImpl.java
+++ src/main/java/com/example/task_flow/service/implementations/UserServiceImpl.java
@@ -1,14 +1,19 @@
+// UserServiceImpl.java
 package com.example.task_flow.service.implementations;
 
-// UserServiceImpl.java
 import com.example.task_flow.mapper.UserMapper;
 import com.example.task_flow.model.dto.UserDTO;
 import com.example.task_flow.model.dto.response.UserResponseDTO;
 import com.example.task_flow.model.entities.User;
 import com.example.task_flow.repository.UserRepository;
 import com.example.task_flow.service.IUserService;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements IUserService {
@@ -25,10 +30,53 @@ public class UserServiceImpl implements IUserService {
     @Override
     public UserResponseDTO createUser(UserDTO userDTO) {
         User user = userMapper.dtoToEntity(userDTO);
-        System.out.println(user);
         User savedUser = userRepository.save(user);
-        System.out.println(savedUser);
         return userMapper.entityToDto(savedUser);
     }
-}
 
+    @Override
+    public UserResponseDTO updateUser(Long userId, UserDTO userDTO) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setUserName(userDTO.getUserName());
+            user.setEmail(userDTO.getEmail());
+            user.setPassword(userDTO.getPassword());
+            user.setTelephone(userDTO.getTelephone());
+            user.setRole(userDTO.getRole());
+            User updatedUser = userRepository.save(user);
+            return userMapper.entityToDto(updatedUser);
+        } else {
+            throw new ValidationException("User not found with ID: " + userId);
+        }
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            userRepository.deleteById(userId);
+        } else {
+            throw new ValidationException("User not found with ID: " + userId);
+        }
+    }
+
+    @Override
+    public UserResponseDTO getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            return userMapper.entityToDto(user);
+        } else {
+            throw new ValidationException("User not found with ID: " + userId);
+        }
+    }
+
+    @Override
+    public List<UserResponseDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(userMapper::entityToDto)
+                .collect(Collectors.toList());
+    }
+}
